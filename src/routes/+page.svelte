7ab5cefa3c8546184a3d5cf4d8801df99b172968
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2024-12-20 17:45:04 +0700

    [add] about route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<h1>This is Heather Do's website</h1>
+<a href="{base}/about">About me</a>
